7aeaa86be315677ef3ea20d36152281ae7eca725
Valentin <valentin.vuky@gmail.com>
2021-09-02 15:51:52 +0200

    update form test

--- cypress/integration/form.ts
+++ cypress/integration/form.ts
@@ -4,10 +4,16 @@ describe("Form", () => {
 
         const form = cy.get("form#new_user");
         //change email and password with yours
-        form.get("input#user_email").should("exist").type("email");
-        form.get("input#user_password").should("exist").type("pass")
+        form.get("input#user_email").should("exist").type(Cypress.env("email"));
+        form.get("input#user_password").should("exist").type(Cypress.env("pass"))
         cy.get("form#new_user").submit();
 
         cy.get("#rendered-article-feed").should("contain","article");
+
+        cy.visit("https://dev.to/settings/profile");
+        cy.get("textarea[id='profile[currently_learning]']").clear().type("Cypress");
+        cy.get(".sticky-footer-form").submit();
+
+        cy.get(".crayons-banner").should("contain","updated");
     })
 })
